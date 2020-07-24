46b9ccf8fe197e654b49ebc41976d9f5149eae78
Jakondo <pah53k@gmail.com>
2020-07-24 12:48:41 +0300

    Added files and changes required for deployment to server

--- social_app/settings.py
+++ social_app/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -128,6 +129,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+# The absolute path to the directory where collectstatic will collect static files for deployment.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
 
 LOGIN_URL = 'login'
@@ -150,3 +154,8 @@ SOCIAL_AUTH_VK_OAUTH2_EXTRA_DATA = [
     # ('photo_100', 'photo_100'),
     # ('link', 'profile_url'),
 ]
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
